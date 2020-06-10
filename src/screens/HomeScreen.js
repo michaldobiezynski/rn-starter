e6bd730016d1ac10c952115a4d9f859058cf4429
michaldobiezynski <dobiezynski@gmail.com>
2020-06-10 08:52:54 +0100

    Navigating Users Between Screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,23 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity, Button } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Hi</Text>;
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View>
+      <Text style={styles.text}>Hi</Text>
+      <Button
+        onPress={() => navigation.navigate("Components")}
+        title="Go to Components Demo"
+      />
+      <Button
+        title="Go to List Demo"
+        onPress={() => navigation.navigate("List")}
+      />
+      {/* <TouchableOpacity onPress={() => navigation.navigate("List")}>
+        <Text>Go to list demo</Text>
+      </TouchableOpacity> */}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
